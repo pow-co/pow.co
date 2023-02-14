b86f652cab04f0436e016777993c2c9ad1683a71
Guillaume De Martino <guillaumedemartino@gmail.com>
2023-02-14 11:35:03 +0100

    basic watch page

--- components/PanelLayout.tsx
+++ components/PanelLayout.tsx
@@ -8,13 +8,13 @@ const PanelLayout = (props: { children: React.ReactNode }) => {
     <div className='bg-primary-300 dark:bg-primary-700/20'>
       <Header/>
       <div className='h-16'/>
-      <div className='grid grid-cols-12'>
-        <div className='hidden lg:block col-span-2 lg:w-full lg:pr-7'>
+      <div className='grid grid-cols-12 sm:pr-5'>
+        <div className='hidden lg:block col-span-2 lg:w-full'>
           <div className='w-20 xl:w-64 fixed top-16 h-[calc(100%-4rem)] z-50'>
             <SideBar/>
           </div>
         </div>
-        <div className='col-span-12 lg:col-span-9 lg:pr-7 min-h-screen'>
+        <div className='col-span-12 lg:col-span-10 min-h-screen'>
           {props.children}
         </div>
       </div>
---
+++ pages/watch.tsx
@@ -0,0 +1,75 @@
+import ThreeColumnLayout from "../components/ThreeColumnLayout"
+import Loader from "../components/Loader"
+import Link from "next/link"
+import { useTuning } from "../context/TuningContext"
+import { useAPI } from "../hooks/useAPI"
+import BoostContentCard from "../components/BoostContentCard";
+import { Ranking } from "../components/BoostContentCard";
+import { useRelay } from "../context/RelayContext"
+import { FormattedMessage } from "react-intl"
+import BitcoinBrowser from "../components/BitcoinBrowser"
+import FindOrCreate from "../components/FindOrCreate"
+import PanelLayout from "../components/PanelLayout"
+import YouTube from "react-youtube"
+import { youtubePlayerOpts } from "../components/YoutubeMetadataOnchain"
+import VideoCard from "../components/VideoCard"
+import TuningPanel from "../components/TuningPanel"
+
+
+
+export default function Watch() {
+  const { startTimestamp, filter, setFilter } = useTuning()
+  const { authenticated } = useRelay()
+  const { data, error, loading } = useAPI(`/boost/rankings?start_date=${startTimestamp}`, '')
+
+  if (loading){
+    return (
+      <>
+      <PanelLayout>
+        <div className="mt-5 lg:mt-10">
+          <Loader/>
+        </div>
+      </PanelLayout>
+      </>
+    )
+  }
+
+  if (error) {
+    return (
+      <PanelLayout>
+        Error, something happened
+      </PanelLayout>
+    )
+  }
+
+  
+  let { rankings } = data 
+  
+  
+  return (
+    <PanelLayout>
+      <div className="mb-[200px] min-h-screen ">
+        <div className="p-5 flex">
+          <FindOrCreate/>
+          <div className="hidden sm:block">
+            <TuningPanel/>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 w-full gap-2 sm:gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {rankings.map((content: any) => {
+            if(content.content_type !== "application/json"){
+              return <></>
+            } else {
+              // if not video, return <></>
+              return <VideoCard key={content.content_txid} difficulty={content.difficulty} txid={content.content_txid}/>
+            }
+          })} 
+          
+        </div>
+
+      </div>
+
+
+    </PanelLayout>
+  )
+}
