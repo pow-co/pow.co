10d792cb94d20d61f85b9fb42317980f8d955004
Elvis Ibarra <ielvisd@gmail.com>
2023-04-10 21:20:29 -0700

    lint-staged test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,51 +1,48 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import Script from 'next/script'
+/* eslint-disable react/jsx-props-no-spreading */
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Script from 'next/script';
 
-import '../styles/globals.css'
-import { ThemeProvider } from 'next-themes'
-import { config } from "../template_config"
-import { RelayProvider } from '../context/RelayContext'
-import { HandCashProvider } from '../context/HandCashContext'
-import { TuneProvider } from '../context/TuningContext'
-import Locales from '../context/LocalContext'
-import { Toaster } from 'react-hot-toast'
-import { BitcoinProvider } from '../context/BitcoinContext'
-import { TwetchProvider } from '../context/TwetchContext'
-import 'react-tooltip/dist/react-tooltip.css'
-
-import { init } from "@socialgouv/matomo-next";
-import { useEffect } from 'react'
-
+import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import { Toaster } from 'react-hot-toast';
+import { init } from '@socialgouv/matomo-next';
+import { useEffect } from 'react';
+import { config } from '../template_config';
+import { RelayProvider } from '../context/RelayContext';
+import { HandCashProvider } from '../context/HandCashContext';
+import { TuneProvider } from '../context/TuningContext';
+import Locales from '../context/LocalContext';
+import { BitcoinProvider } from '../context/BitcoinContext';
+import { TwetchProvider } from '../context/TwetchContext';
+import 'react-tooltip/dist/react-tooltip.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-
   useEffect(() => {
-
     const MATOMO_URL = String(process.env.NEXT_PUBLIC_MATOMO_URL);
-    
+
     const MATOMO_SITE_ID = String(process.env.NEXT_PUBLIC_MATOMO_SITE_ID);
 
     init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
-
   }, []);
 
   return (
     <>
       <Head>
         <title>{config.title}</title>
-        <meta
-          name='description'
-          content={config.description}
-        />
-        <meta name='keywords' content={config.keywords} />
-        <meta name='viewport' content='initial-scale=1, width=device-width' />
+        <meta name="description" content={config.description} />
+        <meta name="keywords" content={config.keywords} />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <Script
         src="https://one.relayx.io/relayone.js"
         strategy="beforeInteractive"
       />
-      <ThemeProvider attribute='class' enableSystem={false} disableTransitionOnChange={true}>
+      <ThemeProvider
+        attribute="class"
+        enableSystem={false}
+        disableTransitionOnChange
+      >
         <RelayProvider>
           <TwetchProvider>
             <HandCashProvider>
@@ -53,14 +50,14 @@ export default function App({ Component, pageProps }: AppProps) {
                 <TuneProvider>
                   <Locales>
                     <Component {...pageProps} />
-                    <Toaster/>
+                    <Toaster />
                   </Locales>
                 </TuneProvider>
               </BitcoinProvider>
             </HandCashProvider>
-          </TwetchProvider>        
+          </TwetchProvider>
         </RelayProvider>
       </ThemeProvider>
     </>
-  )
+  );
 }
