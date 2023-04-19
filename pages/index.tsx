43eb2f6b31a50fabcafc44e880c70cac8eba4ed9
Guillaume De Martino <guillaumedemartino@gmail.com>
2023-04-19 19:15:22 +0200

    use powco/feeds/multi-day endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,92 +1,102 @@
-import ThreeColumnLayout from "../components/ThreeColumnLayout"
-import Loader from "../components/Loader"
-import Link from "next/link"
-import { useTuning } from "../context/TuningContext"
-import { useAPI } from "../hooks/useAPI"
-import BoostContentCard from "../components/BoostContentCard";
-import { Ranking } from "../components/BoostContentCard";
-import { useRelay } from "../context/RelayContext"
-import { FormattedMessage } from "react-intl"
-import BitcoinBrowser from "../components/BitcoinBrowser"
-import FindOrCreate from "../components/FindOrCreate"
-import { useBitcoin } from "../context/BitcoinContext"
-import CardErrorBoundary from "../components/CardErrorBoundary"
-
-
+import Link from 'next/link';
+import ThreeColumnLayout from '../components/ThreeColumnLayout';
+import Loader from '../components/Loader';
+import { useTuning } from '../context/TuningContext';
+import { useAPI } from '../hooks/useAPI';
+import BoostContentCard, { Ranking } from '../components/BoostContentCard';
+import FindOrCreate from '../components/FindOrCreate';
+import { useBitcoin } from '../context/BitcoinContext';
+import CardErrorBoundary from '../components/CardErrorBoundary';
 
 export default function Home() {
-  const { startTimestamp, filter, setFilter } = useTuning()
-  const { authenticated } = useBitcoin()
-  const { data, error, loading } = useAPI(`/boost/rankings/${filter}`, '')
+  const { filter } = useTuning();
+  const { authenticated } = useBitcoin();
+  const { data, error, loading } = useAPI(filter === 'last-day' ? '/powco/feeds/multi-day' : `/boost/rankings/${filter}`, '');
 
   if (error) {
     return (
       <ThreeColumnLayout>
         Error, something happened
       </ThreeColumnLayout>
-    )
+    );
   }
 
-  
-  let { rankings } = data || []
+  const { rankings } = data || [];
+  const { days } = data || [];
 
-  //console.log("RANKINGS", rankings)
-  
+  // console.log("RANKINGS", rankings)
+
   return (
-    <>
     <ThreeColumnLayout>
-      {authenticated && <div className="mt-5 sm:mt-10">
-        <FindOrCreate/>
-      </div>}
-      <div className="flex mt-5 mx-0 px-4">
-        <Link href={`/`}>
-          <div className="text-sm leading-4 py-2 px-3 text-gray-900 dark:text-white bg-primary-100 dark:bg-primary-600/20 font-medium mr-2 cursor-pointer rounded-md whitespace-nowrap">
+      {authenticated && (
+      <div className="mt-5 sm:mt-10">
+        <FindOrCreate />
+      </div>
+      )}
+      <div className="mx-0 mt-5 flex px-4">
+        <Link href="/">
+          <div className="mr-2 cursor-pointer whitespace-nowrap rounded-md bg-primary-100 px-3 py-2 text-sm font-medium leading-4 text-gray-900 dark:bg-primary-600/20 dark:text-white">
             All
           </div>
         </Link>
-        <Link href={`/relayx`}>
-          <div className="text-sm leading-4 py-2 px-3 text-gray-700 dark:text-gray-300 font-normal mr-2 cursor-pointer rounded-md whitespace-nowrap">
+        <Link href="/relayx">
+          <div className="mr-2 cursor-pointer whitespace-nowrap rounded-md px-3 py-2 text-sm font-normal leading-4 text-gray-700 dark:text-gray-300">
             Club RelayX
           </div>
         </Link>
-        <Link href={`/twetch`}>
-          <div className="text-sm leading-4 py-2 px-3 text-gray-700 dark:text-gray-300 font-normal mr-2 cursor-pointer rounded-md whitespace-nowrap">
+        <Link href="/twetch">
+          <div className="mr-2 cursor-pointer whitespace-nowrap rounded-md px-3 py-2 text-sm font-normal leading-4 text-gray-700 dark:text-gray-300">
             Twetch
           </div>
         </Link>
       </div>
-      <div className="col-span-12 lg:col-span-6 min-h-screen">
-        <div className="mt-5 lg:mt-10 mb-[200px]">
-          {loading ? <Loader/> : rankings?.map((post: Ranking) => {
-            return (
-             <CardErrorBoundary key={post.content_txid}>
-               <BoostContentCard  {...post}/>
-               </CardErrorBoundary>
-             )
-          } ) }
+      <div className="col-span-12 min-h-screen lg:col-span-6">
+        <div className="mb-[200px] mt-5 lg:mt-10">
+          {loading ? <Loader /> : (
+            <>
+              {rankings?.map((post: Ranking) => (
+                <CardErrorBoundary key={post.content_txid}>
+                  <BoostContentCard {...post} />
+                </CardErrorBoundary>
+              ))}
+              {days?.map((daysRankings: Ranking[], index: number) => (
+                <div key={`ranking_days_${index + 1}`}>
+                  <div className="flex items-center py-5">
+                    <div className="border-bottom grow border border-gray-600 dark:border-gray-300" />
+                    <div className="mx-5 text-lg font-semibold text-gray-600 dark:text-gray-300">{`${index + 1} ${index + 1 > 1 ? 'days' : 'day'} before`}</div>
+                    <div className="border-bottom grow border border-gray-600 dark:border-gray-300" />
+                  </div>
+                  {daysRankings?.map((daysPost: Ranking) => (
+                    <CardErrorBoundary key={daysPost.content_txid}>
+                      <BoostContentCard {...daysPost} />
+                    </CardErrorBoundary>
+                  ))}
+                </div>
+              ))}
+            </>
+          )}
         </div>
       </div>
       {authenticated && (
-            <Link href="/compose">
-              <div className=" lg:hidden fixed bottom-[73px] right-[14px] h-14 w-14 rounded-full bg-gradient-to-r from-blue-400 to-blue-500 flex items-center justify-center cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 text-white"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4v16m8-8H4"
-                  />
-                </svg>
-              </div>
-            </Link>
-        )}
+      <Link href="/compose">
+        <div className=" fixed bottom-[73px] right-[14px] flex h-14 w-14 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-blue-400 to-blue-500 lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4v16m8-8H4"
+            />
+          </svg>
+        </div>
+      </Link>
+      )}
     </ThreeColumnLayout>
-    </>
-  )
+  );
 }
