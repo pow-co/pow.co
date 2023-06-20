764ea7ab4c0fec6ff9ffd4dfca21c7a4a53d261d
usselman <sarkar.shirish@gmail.com>
2023-06-20 02:59:51 -0700

    fixed sketch rendering + updating tuning

--- components/VisualizerPanel.tsx
+++ components/VisualizerPanel.tsx
@@ -1,8 +1,8 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import axios from "axios";
+import useSWR from "swr";
 import dynamic from "next/dynamic";
-import { useRelay } from "../context/RelayContext";
 import { useTuning } from "../context/TuningContext";
 import TuningPanel from "./TuningPanel";
 import { cleanString, Ranking } from "../pages/topics";
@@ -11,54 +11,55 @@ const CircleSketch = dynamic(() => import("./visualizer/CircleSketch"), {
   ssr: false, // Disable server-side rendering
 });
 
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+
 const VisualizerPanel = () => {
   const { startTimestamp } = useTuning();
-  const [loading, setLoading] = useState<boolean>(false);
+  const { data, error } = useSWR(
+    `https://pow.co/api/v1/boost/rankings/tags?start_date=${startTimestamp}`,
+    fetcher
+  );
   const [tags, setTags] = useState<Ranking[]>([]);
   const [maxDifficulty, setMaxDifficulty] = useState<number>(0);
 
   useEffect(() => {
-    setLoading(true);
-    axios
-      .get(
-        `https://pow.co/api/v1/boost/rankings/tags?start_date=${startTimestamp}`
-      )
-      .then(({ data }: { data: any }) => {
-        const tags = data.rankings
-          .map((ranking: Ranking) => {
-            var tag = ranking.tag;
+    if (data) {
+      const tags = data.rankings
+        .map((ranking: Ranking) => {
+          var tag = ranking.tag;
 
-            try {
-              tag = Buffer.from(ranking.tag, "hex").toString();
-              tag = cleanString(tag);
-            } catch (error) {
-              tag = "";
-            }
+          try {
+            tag = Buffer.from(ranking.tag, "hex").toString();
+            tag = cleanString(tag);
+          } catch (error) {
+            tag = "";
+          }
 
-            return Object.assign(ranking, {
-              tag,
-            });
-          })
-          .filter((tag: any) => !/\s/.test(tag.tag))
-          .filter((tag: any) => !/[&\/\\#,()$~%'":?<>{}]/.test(tag.tag))
-          .filter(
-            (tag: any) =>
-              tag.tag !=
-              "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-          )
-          .filter((tag: any) => tag.tag != "");
+          return Object.assign(ranking, {
+            tag,
+          });
+        })
+        .filter((tag: any) => !/\s/.test(tag.tag))
+        .filter((tag: any) => !/[&\/\\#,()$~%'":?<>{}]/.test(tag.tag))
+        .filter(
+          (tag: any) =>
+            tag.tag !=
+            "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        )
+        .filter((tag: any) => tag.tag != "");
 
-        // Set the maximum difficulty
-        const maxDifficulty = Math.max(
-          ...tags.map((tag: Ranking) => tag.difficulty)
-        );
+      // Set the maximum difficulty
+      const maxDifficulty = Math.max(
+        ...tags.map((tag: Ranking) => tag.difficulty)
+      );
 
-        setTags(tags);
-        setMaxDifficulty(maxDifficulty);
+      setTags(tags);
+      setMaxDifficulty(maxDifficulty);
+    }
+  }, [data]);
 
-        setLoading(false);
-      });
-  }, [startTimestamp]);
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
 
   return (
     <div className="bg-primary-100 dark:bg-primary-600/20 min-h-60">
--- pages/galaxy.tsx
+++ pages/galaxy.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import VisualizerPanel from "../components/VisualizerPanel";
-import Link from "next/link";
 
 const Galaxy = () => {
   return <VisualizerPanel />;
