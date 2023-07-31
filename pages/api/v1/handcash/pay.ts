a6a25cb0abce0315fc2f0984997a39914e1d0f87
Owen Kellogg <508282+owenkellogg@users.noreply.github.com>
2023-07-31 16:26:31 +0000

    API to Pay to Script with Handcash authToken

--- pages/api/v1/handcash/pay.ts
+++ pages/api/v1/handcash/pay.ts
@@ -1,30 +1,120 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import AuthTokenRepository from '../../../../repositories/AuthTokenRepository';
-import HandCashService from '../../../../services/HandCashService';
-import SessionTokenRepository from '../../../../repositories/SessionTokenRepository';
+
+import axios from 'axios';
+
+import { bsv } from 'scrypt-ts';
+
+import { HandCashConnect } from '@handcash/handcash-connect';
+
+import delay from 'delay';
+
+const appId: string = String(process.env.handcash_app_id);
+const appSecret: string = String(process.env.handcash_app_secret);
+
+const handCashConnect = new HandCashConnect({
+  appId,
+  appSecret,
+});
+
+async function createScriptShortcode({ script }: { script: bsv.Script }): Promise<string> {
+  const { data } = await axios.post('https://pow.co/api/v1/script-shortcodes', {
+    script: script.toHex(),
+  });
+
+  return data.uid;
+}
+
+interface FundScript {
+  script: bsv.Script;
+  value: bigint;
+  authToken: string;
+  note?: string;
+}
+
+async function fundScript({
+  script, value, authToken, note, 
+}: FundScript): Promise<{ txid: string, txhex?: string }> {
+  const account = handCashConnect.getAccountFromAuthToken(authToken);
+
+  const sendAmount = Number(value) * 1e-8;
+
+  const currencyCode = 'BSV';
+
+  const shortCode = await createScriptShortcode({ script });
+
+  const destination = `${shortCode}@pow.co`;
+
+  const payment = {
+    destination, currencyCode, sendAmount,
+  };
+
+  const { transactionId: txid } = await account.wallet.pay({
+    note: note || '',
+    // @ts-ignore TODO: Figure out why CurrencyCode is not assignable to string (typescript compilation error)
+    payments: [payment],
+  });
+
+  let txhex: string;
+
+  try {
+    txhex = await reliableFetchTransaction({ txid });
+
+    return { txid, txhex };
+  } catch (error) {
+    console.error(error);
+
+    return { txid };
+  }
+}
+
+async function fetchTransaction({ txid }: { txid: string }): Promise<string> {
+  const resp = await axios.get(`https://api.whatsonchain.com/v1/bsv/main/tx/${txid}/hex`);
+
+  return resp.data;
+}
+
+async function reliableFetchTransaction({ txid }: { txid: string }): Promise<string> {
+  let tries = 0;
+
+  let txhex: string;
+
+  let success = false;
+
+  while (tries < 5 && !success) {
+    try {
+      txhex = await fetchTransaction({ txid });
+
+      success = true;
+
+      return txhex;
+    } catch (error) {
+      tries += 1;
+
+      await delay(tries * 1000);
+    }
+  }
+
+  throw new Error('failed to fetch transaction');
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(404);
   }
-  try {
-    const { authorization } = req.headers;
-    const sessionToken = authorization?.split(' ')[1];
-    if (!sessionToken) {
-      return res.status(401).json({ error: 'Missing authorization.' });
-    }
 
-    const { sessionId, user } = SessionTokenRepository.verify(sessionToken);
-    const authToken = AuthTokenRepository.getById(sessionId);
-    if (!authToken) {
-      return res.status(401).json({ status: 'error', error: 'Expired authorization.' });
-    }
-    const paymentResult = await new HandCashService(authToken).pay({
-      destination: user.handle, amount: 0.05, currencyCode: 'USD',
+  try {
+    const { authToken, script, value } = req.body;
+
+    const { txid, txhex } = await fundScript({
+      authToken,
+      value: BigInt(value),
+      script: bsv.Script.fromHex(script),
     });
-    return res.status(200).json({ status: 'sent', transactionId: paymentResult.transactionId });
+
+    return res.status(200).json({ status: 'sent', txid, txhex });
   } catch (error: any) {
     console.error(error);
+
     return res.status(400).json({ status: 'error', message: error.toString() });
   }
 }
